60754751c713f465b49b42b5f98d077214f1d4a0
Pierre <pierre.petitiot@gmail.com>
2023-04-20 10:52:07 +0200

    20/04/2023

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Form\UserPasswordType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -48,7 +49,7 @@ class UserController extends AbstractController
                 // !!!!! Message flash : Modification profil utilisateur !!!!!
                 $this->addFlash    // Nécessite un block "for message" dans index.html.twig pour fonctionner
                 (
-                    'warning',  // Nom de l'alerte 
+                    'success',  // Nom de l'alerte 
                     ['info' => 'Modification !','bonus' => "Le compte utilsateur M./Mme \"" . $user->getFullName() . "\" a bien été modifié"]  // Message(s)
                 );
 
@@ -62,13 +63,62 @@ class UserController extends AbstractController
                     'danger',  // Nom de l'alerte 
                     ['info' => 'Erreur !','bonus' => "Le mot de passe renseigné est incorrect"]  // Message(s)
                 );
-            }
-
-            
+            }  
         }
 
         return $this->render('pages/user/edit.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/utilisateur/edition-mot-de-passe/{id}', name: 'user.edit.password', methods: ['GET', 'POST'])]
+    public function editPassword(User $user, Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $manager) : Response
+    {
+        if (!$this->getUser()) 
+        {
+            return $this->redirectToRoute('security.login');
+        }
+
+        if ($this->getUser() !== $user)
+        {
+            return $this->redirectToRoute('recipe.index');
+        }
+        
+        $form = $this->createForm(UserPasswordType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            if ($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) 
+            {
+                $user->setPassword($hasher->hashPassword($user, $form->getData()['newPassword']));
+
+                $manager->persist($user);
+                $manager->flush();
+
+                // !!!!! Message flash : Modification mot de passe utilisateur !!!!!
+                $this->addFlash    // Nécessite un block "for message" dans index.html.twig pour fonctionner
+                (
+                    'success',  // Nom de l'alerte 
+                    ['info' => 'Modification !','bonus' => "Le mot de passe de M./Mme \"" . $user->getFullName() . "\" a bien été modifié"]  // Message(s)
+                );
+
+                return $this->redirectToRoute('recipe.index');
+            }
+            else
+            {
+                // !!!!! Message flash : Modification profil utilisateur !!!!!
+                $this->addFlash    // Nécessite un block "for message" dans index.html.twig pour fonctionner
+                (
+                    'danger',  // Nom de l'alerte 
+                    ['info' => 'Erreur !','bonus' => "Le mot de passe renseigné est incorrect"]  // Message(s)
+                );
+            }  
+        }
+
+        return $this->render('pages/user/edit_password.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
